ffadf8029e0743afadc8ef7ca7394efa72f57dbc
thurssdar <145630790+thurssdar@users.noreply.github.com>
2023-12-14 11:53:08 +0700

    penyesuaian index,navbar,dan sidebar

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2023 <a href="#">Dara Shalsabila Furda</a>.</strong> All rights reserved.
+  </footer>
